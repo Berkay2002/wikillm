33f023f5c4d4db7941d6dadcd758ef06c0b927b1
Berkay Orhan <berkayorhan@hotmail.se>
2026-04-06 15:12:20 +0200

    feat: add chalk-based logger utility

---
+++ src/utils/logger.ts
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+export const log = {
+  info: (msg: string) => console.log(chalk.blue("  " + msg)),
+  success: (msg: string) => console.log(chalk.green("  ✓ " + msg)),
+  warn: (msg: string) => console.log(chalk.yellow("  ⚠ " + msg)),
+  error: (msg: string) => console.log(chalk.red("  ✗ " + msg)),
+  step: (msg: string) => console.log(chalk.dim("  " + msg)),
+  title: (msg: string) => console.log("\n" + chalk.bold(msg)),
+};
